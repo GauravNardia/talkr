78daf764a037e90a7946db7ca9fd6c062651aabd
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-16 11:45:07 +0530

    add more schema to postgres

--- database/schema.ts
+++ database/schema.ts
@@ -23,6 +23,8 @@ export const users = pgTable("users", {
     nativeLanguage: text("native_language"),
     targetLanguage: text("target_language"),
     onboarded: boolean("onboarded").default(false),
+    totalXp: integer("total_xp").default(0),
+    level: integer("level").default(1),
     streakCount: integer("streak_count").default(1),
     longestStreak: integer("longest_streak").default(1),
     lastActivityDate: date("last_activity_date").defaultNow(),
@@ -41,6 +43,33 @@ export const subscriptions = pgTable("subscription", {
     createdAt: timestamp("createdAt", { mode: "date" }).defaultNow().notNull(),
     updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow().notNull(),
 });
+
+export const quizzes = pgTable("quizzes", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").notNull().unique().references(() => users.id),
+  question: text("question").notNull(),
+  answer: text("answer").notNull(),
+  options: text("options").notNull(),
+  isCorrect: boolean("isCorrect"),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
+
+
+export const xps = pgTable('xp', {
+   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+   userId: uuid("userId").notNull().unique().references(() => users.id),
+   xp: integer("xp").default(0),
+   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow().notNull(),
+   updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow().notNull(),
+});
+
+export const leaderboards = pgTable("leaderboard", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").notNull().unique().references(() => users.id),
+  weekStart: date("weekStart"),
+  totalXp: integer("totalXp"),
+});
+
 
   export const userRelations = relations(users, ({ one }) => ({
     subscription: one(subscriptions, {
